457d17efb10b0978201387201ab2b6e3cb79844b
Dimitris Christodoulou <dimitris.christodoulou.dev@gmail.com>
2017-09-05 12:15:07 +0300

    Minor changes

--- frontend/js/createThesis.js
+++ frontend/js/createThesis.js
@@ -1,8 +1,60 @@
 $(function () {
 
-        $('.wysiwyg_editor').summernote();
+    var HelloButton = function (context) {
+        var ui = $.summernote.ui;
+
+        // create button
+        var button = ui.button({
+            contents: '<i class="fa fa-save"/>',
+            tooltip: 'Save to DB',
+            click: function () {
+                // invoke insertText method with 'hello' on editor module.
+                // context.invoke('editor.insertText', 'hello');
+                alert('asdaasd');
+            }
+        });
+
+        return button.render();   // return button as jquery object
+    }
+
+
+    $('.wysiwyg_editor').summernote({
+            placeholder: 'Write here...',
+            dialogsInBody: true,
+            dialogsFade: true,  // Add fade effect on dialogs
+            toolbar: [
+                // [groupName, [list of button]]
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough', 'superscript', 'subscript']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['style','ul', 'ol', 'paragraph','height']],
+                ['insert',['picture','link','video','hr']],
+                ['misc', ['fullscreen','codeview','undo','redo','help','save']],
+                ['saveBtn',['']]
+            ],
+            popover: {
+                air: [
+                    ['color', ['color']],
+                    ['font', ['bold', 'underline', 'clear']]
+                ]
+            },
+            buttons:{
+                'save': HelloButton
+            }
+            }
+        ).on('summernote.change', function(customEvent, contents, $editable) {
+            var markupStr = $(this).summernote('code');
+            $('#view').html(markupStr)
+            // $('#summernoteForm').formValidation('revalidateField', 'content');
+        });
+
+
+
+
+
+
         createTypeahead()
-        $('.wysiwyg_editor').on('summernote.keyup', refreshView)
 })
 
 function createTypeahead() {
@@ -39,18 +91,19 @@ function createTypeahead() {
         'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
     ];
 
-    $('.typeahead').typeahead({
+    var typeahead = $('#the-basics.typeahead').typeahead({
             hint: true,
             highlight: true,
-            minLength: 1
+            minLength: 2
+
         },
         {
             name: 'states',
             source: substringMatcher(states)
         });
+    typeahead.unwrap();
 }
 
 function refreshView() {
-    var markupStr = $(this).summernote('code');
-    $('#view').html(markupStr)
+
 }
